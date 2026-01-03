9e9baeadf557f765c02b8694d6f9f35f96dba96f
ely colmenarez <elymelecc084@gmail.com>
2026-01-03 13:15:08 -0400

    realizando mas pruebas

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/admin/',//'https://tienda-eta-three.vercel.app/api/admin/',
+  baseURL: 'https://tienda-eta-three.vercel.app/api/admin/',
   headers: {
     'Content-Type': 'application/json',
   },
