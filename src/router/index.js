8a58a9c0b991a138ca1380ecbec1f8e6e386f44d
ely colmenarez <elymelecc084@gmail.com>
2026-01-02 21:42:04 -0400

    usando en las rutas el createWebHashistori para que funcione con github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router';
+//import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { useAuthStore } from '../stores/auth';
 
 // Importa tus componentes de vista
@@ -26,7 +27,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
 
